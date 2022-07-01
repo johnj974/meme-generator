58e4650c3e9b3110436fe3e9de2e9d6490d95c9b
johnj974 <jjdug@hotmail.com>
2022-07-01 16:52:36 +0100

    simpleFormAdded

--- src/Meme.js
+++ src/Meme.js
@@ -7,6 +7,18 @@ export default function Meme() {
   const memeArray = memesData.data.memes;
 
   const [meme, setMeme] = React.useState("https://i.imgflip.com/wxica.jpg");
+  const [textInputs, setTextInputs] = React.useState({
+    topText: "",
+    bottomText: "",
+  });
+
+  function createText(event) {
+    const { name, value } = event.target;
+    setTextInputs((prevText) => ({
+      ...prevText,
+      [name]: value,
+    }));
+  }
 
   function generate() {
     const number = Math.round(Math.random() * memeArray.length);
@@ -17,17 +29,31 @@ export default function Meme() {
   return (
     <div className="section">
       <form className="form">
-        <input type="text" className="inputOne" placeholder="top-text"></input>
+        <input
+          type="text"
+          className="inputOne"
+          placeholder="top-text"
+          name="topText"
+          value={textInputs.topText}
+          onChange={createText}
+        ></input>
         <input
           type="text"
           className="inputTwo"
           placeholder="bottom-text"
+          name="bottomText"
+          value={textInputs.bottomText}
+          onChange={createText}
         ></input>
       </form>
       <button className="generateButton" onClick={generate}>
         Generate Meme
       </button>
-      <img alt="meme" src={meme} />
+      <div className="meme-display">
+        <img alt="meme" src={meme} />
+        <p className="top text">{textInputs.topText}</p>
+        <p className="bottom text">{textInputs.bottomText}</p>
+      </div>
     </div>
   );
 }
